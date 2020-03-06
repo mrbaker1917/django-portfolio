49bf0061d1926bfc429fd359e0e0f31d9336d559
mrbaker1917 <mrbaker1917@gmail.com>
2020-03-05 19:43:49 -0800

    more blog work for 2020-03-05

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -10,6 +13,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
